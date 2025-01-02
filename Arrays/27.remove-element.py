76a1513171d283a9db0fe1294cc5e9f1211e6b4b
Anushreearora <104303593+Anushreearora@users.noreply.github.com>
2025-01-02 22:41:22 +0100

    remove element

--- Arrays/27.remove-element.py
+++ Arrays/27.remove-element.py
@@ -12,12 +12,18 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-
-        
-
-
-
-
+        i=0
+        j=len(nums)-1
+        while(i<=j):
+            if nums[i] ==val and nums[j]!=val:
+                nums[i],nums[j]=nums[j],nums[i]
+                i+=1
+                j-=1
+            if nums[j]==val:
+                j-=1
+            if nums[i]!=val:
+                i+=1
+        return i
 
 # @lc code=end
 
